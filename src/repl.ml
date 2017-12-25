476356884ea98f98cacb77014c685c0291d07d48
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-12-25 01:04:01 +0100

    Passing editor content to repl.

--- src/repl.ml
+++ src/repl.ml
@@ -1,10 +1,11 @@
-let echoer f (s : string) : unit =
-  Js.Unsafe.fun_call f [| Js.Unsafe.inject (Js.number_of_float 42.0) |]
+let echoer f (msg : string) : unit =
+  Js.Unsafe.fun_call f [| Js.Unsafe.inject (Js.bytestring msg) |]
 
 let _ =
   Js.export "repl"
   (object%js
-     method toplevel s f =
-       echoer f "It might work" ;
-       Js.bytestring (Lang.toplevel (Js.to_bytestring s))
+     method toplevel s f = Js.bytestring (Lang.toplevel (Js.to_bytestring s))
+     method commands s f =
+       let s = Js.to_bytestring s in
+       echoer f (Format.sprintf "We got %d characters.@." (String.length s))
    end)
